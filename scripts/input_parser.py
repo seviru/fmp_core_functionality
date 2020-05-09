ed0616fc0a81927380f72337e8bd96b8f554df27
seviru <sebastian.vruibal@gmail.com>
2020-05-09 22:38:02 +0200

    Removed intermediate tests and use of functions

--- scripts/input_parser.py
+++ scripts/input_parser.py
@@ -25,7 +25,7 @@ outfile_args.add_argument("-o", "--tree_outfile", type=str, default="tree_image.
 outfile_args.add_argument("-n", "--node_score_table", type=str, default="node_score.tsv",
                     help="File where we will store the node scores for our tree.")
 parameter_args.add_argument("-c", "--calculus_algorithm", type=str, default="simple", 
-                    choices=["simple", "all_vs_all", "whole_annotation_simple", "whole_annotation_all_vs_all", "all_vs_all_means", "whole_annotation_all_vs_all_means"]
+                    choices=["simple", "all_vs_all", "whole_annotation_simple", "whole_annotation_all_vs_all", "all_vs_all_means", "whole_annotation_all_vs_all_means"],
                     help="Select which calculation algorithm you want to utilize to calculate the node scores in your tree.")
 parameter_args.add_argument("-g", "--ignore_gap_positions", type=str, default="N", choices=["Y", "N"],
                     help="Decide if you want your calculus algorithm to ignore positions with gaps. At the moment, only 'simple'\
@@ -35,25 +35,14 @@ parameter_args.add_argument("-e", "--min_evalue", type=float, default=1e-10,
 parameter_args.add_argument("-f", "--annotation_feature", type=str, default="ALL",
                     help="Feature for which we want to represent our tree or get the node scores.")
 
-
-
 args = parser.parse_args()
 
-###AQUI CREO LA INSTANCIA DE LA CLASE Y LLAMO A SUS METODOS
+
 case_study = main.FeatureStudy(args.tree_infile, args.alignment_infile, args.table_infile,
                            args.uniprot_infile, args.annotation_feature, args.min_evalue,
                            args.tree_outfile, args.node_score_table, args.calculus_algorithm,
                            args.ignore_gap_positions)
-
-# print(case_study.study_features)
-# case_study.calculate_nodes()
-
-# case_study.design_tree()
-
-# case_study.processed_tree.render(case_study.tree_out, w=800, h=2000, units="px", tree_style=ts)
-# ts = TreeStyle()
-# ts.layout_fn = lambda x: True
-
 case_study.plot_tree()
+
 
 ## END
