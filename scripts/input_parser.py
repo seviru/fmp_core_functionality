51e0b832dfb0e9f42a33ca40e8d2a205229f1902
seviru <sebastian.vruibal@gmail.com>
2020-05-10 19:47:26 +0200

    Finished main script to call funcionality

--- scripts/input_parser.py
+++ scripts/input_parser.py
@@ -4,6 +4,7 @@ will launch different functionalities in the main script
 """
 import argparse
 import src.main_class as main
+import sys
 
 parser = argparse.ArgumentParser(description="Handles the desired output information by the user and asks for the required input files.")
 infile_args = parser.add_argument_group("INFILES", "Files of input data for our script to process.")
@@ -19,10 +20,10 @@ infile_args.add_argument("-t", "--table_infile", type=str, required=True,
                     help="Table file with the required format.")
 infile_args.add_argument("-u", "--uniprot_infile", type=str, required=True,
                     help="File containing the uniprot annotations for our data cluster.")
-outfile_args.add_argument("-o", "--tree_outfile", type=str, default="tree_image.png",
+outfile_args.add_argument("-o", "--tree_outfile", type=str,
                     help="File where we will save the image of our phylogenetic tree with the secuence in the specified annotation\
                           and the node scores.")
-outfile_args.add_argument("-n", "--node_score_table", type=str, default="node_score.tsv",
+outfile_args.add_argument("-n", "--node_score_table", type=str,
                     help="File where we will store the node scores for our tree.")
 parameter_args.add_argument("-c", "--calculus_algorithm", type=str, default="simple", 
                     choices=["simple", "all_vs_all", "whole_annotation_simple", "whole_annotation_all_vs_all", "all_vs_all_means", "whole_annotation_all_vs_all_means"],
@@ -37,12 +38,25 @@ parameter_args.add_argument("-f", "--annotation_feature", type=str, default="ALL
 
 args = parser.parse_args()
 
+if args.tree_outfile is None and args.node_score_table is None:
+      sys.stderr.write("Not specified any outfile. Specify at least tree_outfile or node_score_table.\n")
+      sys.exit(1)
 
+print("Creating study instance...")
 case_study = main.FeatureStudy(args.tree_infile, args.alignment_infile, args.table_infile,
                            args.uniprot_infile, args.annotation_feature, args.min_evalue,
-                           args.tree_outfile, args.node_score_table, args.calculus_algorithm,
-                           args.ignore_gap_positions)
-case_study.plot_tree()
+                           args.calculus_algorithm, args.ignore_gap_positions)
 
+print("Designing tree.")
+case_study.design_tree()
+
+if args.tree_outfile is not None:
+      print("Plotting tree.")
+      case_study.plot_tree(outfile=f"{args.tree_outfile}.png", tree=case_study.processed_tree)
+if args.node_score_table is not None:
+      print("Writing nodes to a file.")
+      case_study.write_node_file(outfile=args.node_score_table)
+
+print("Study finished.")
 
 ## END
