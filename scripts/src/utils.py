c0e862faf3bf37a2050c703bbeddf1d23d69ebb0
seviru <sebastian.vruibal@gmail.com>
2020-08-04 23:57:05 +0200

    Added small function to sort a dictionary by its values

--- scripts/src/utils.py
+++ scripts/src/utils.py
@@ -12,11 +12,22 @@ def dict_diff(a, b):
 def bytefile_to_stringfile(bytefile):
     """You input a byte coded file and it returns it as a string.
     """
-    stringfile = str()
-    for line in bytefile:
-        line = line.decode("utf-8")
-        stringfile = stringfile + line
-    stringfile = stringfile.rstrip()
-    
+    try:
+        stringfile = str()
+        for line in bytefile:
+            line = line.decode("utf-8")
+            stringfile = stringfile + line
+        stringfile = stringfile.rstrip()
+    except:
+        sys.stderr.write("Error at converting a byte file to a string.\n")
+        sys.exit(1)
+
     return stringfile
+
+def sort_dict_byvalue(dictionary):
+    """Sorts a dictionary by value, decreseangly by number.
+    """
+    return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1])}
+
+
 ## END
